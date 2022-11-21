2e7eeaf04801c4684e13a393c1e8247641e687f6
antoniosanc <chucho2450@gmail.com>
2022-11-20 22:30:19 -0600

    index

--- login.php
+++ login.php
@@ -15,7 +15,7 @@
   <link rel="shortcut icon" type="image/x-icon" href="img/fav.ico">
 
 </head>
-<body>
+<body class="background-radial-gradient">
 <style>
     .background-radial-gradient {
       background-color: hsl(218, 41%, 15%);
@@ -33,34 +33,11 @@
           transparent 100%);
     }
 
-    #radius-shape-1 {
-      height: 220px;
-      width: 220px;
-      top: -60px;
-      left: -130px;
-      background: radial-gradient(#44006b, #ad1fff);
-      overflow: hidden;
-    }
-
-    #radius-shape-2 {
-      border-radius: 38% 62% 63% 37% / 70% 33% 67% 30%;
-      bottom: -60px;
-      right: -110px;
-      width: 300px;
-      height: 300px;
-      background: radial-gradient(#44006b, #ad1fff);
-      overflow: hidden;
-    }
-
-    .bg-glass {
-      background-color: hsla(0, 0%, 100%, 0.9) !important;
-      backdrop-filter: saturate(200%) blur(25px);
-    }
   </style>
 	<!-- Section: Design Block -->
-<section class="background-radial-gradient overflow-hidden">
-  <div class="container py-5 h-100" style="padding-bottom: 2rem!important;">
-    <div class="row d-flex justify-content-center align-items-center h-100">
+<section>
+  <div class="container py-5">
+    <div class="row d-flex justify-content-center align-items-center ">
       <div class="col-12 col-md-8 col-lg-6 col-xl-5">
         <div class="card shadow-2-strong" style="border-radius: 1rem;">
           <div class="card-body p-5" >
@@ -70,7 +47,7 @@
             </div>
             
             <br>
-            <form action="index.php">
+            <form action="index.php" method="post">
             	<div class="form-outline mb-4">
             		<label>Ingresa correo electrónico:</label>
 	            	<input type="email" id="typeEmailX-2" class="form-control form-control-lg" placeholder="Correo electrónico" />
@@ -81,9 +58,8 @@
 	              	<input type="password" id="typePasswordX-2" class="form-control form-control-lg" placeholder="Contraseña" />
 	              
 	            </div>
-
 	            <!-- Checkbox -->
-	            <div class="form-check d-flex justify-content-start mb-4">
+	            <div class="form-check d-flex mb-4">
 	              <input class="form-check-input" type="checkbox" value="" id="form1Example3" />
 	              <label class="form-check-label" for="form1Example3">Recordar contraseña </label>
 	            </div>
@@ -91,13 +67,17 @@
 	            <button class="btn btn-primary btn-lg btn-block" type="submit">Iniciar</button>
             </form>
           </div>
+          <div class="card-footer p-4" >
+          	
+          </div>
         </div>
       </div>
     </div>
   </div>
+
 </section>
 
-<footer class="py-4 bg-light mt-auto">
+<footer class="py-4 bg-light">
     <div class="container-fluid">
         <div class="d-flex align-items-center justify-content-between small">
             <div class="text-muted">Copyright © Todos los derechos reservados SERVICIOS TIC, S.A de C.V   |<a href="mailto:erojano@serviciostic.com.mx" style="text-decoration:none; color: #4750DC;" target="_blank"><b> Developed by ASER</b></a>
@@ -107,8 +87,6 @@
 	</div>
 </footer>
 </body>
-
-
 <!-- jQuery -->
 <script src="plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
